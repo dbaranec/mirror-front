16d5d2b7a56db1f8c114c77b5cc667e604d8e6c5
Dominik Baranec <dominik.baranec@scr.sk>
2019-02-27 20:50:55 +0100

    Add reload component to 10min

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    setInterval(() => this.reloadPage(), 600000);
+
+  }
+
+  private reloadPage() {
+    window.location.reload(true);
+  }
 }
