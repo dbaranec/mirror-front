9e012833722c12243a2c54afa8eb0e45001da971
Dominik Baranec <dominik.baranec@scr.sk>
2020-02-16 12:36:30 +0100

    [*] format date, add moment.js

--- src/app/nameday/nameday.component.ts
+++ src/app/nameday/nameday.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NamedayService} from '../shared/nameday/nameday.service';
 import {NameDayData} from '../shared/nameday/NameDayData';
+import * as moment from 'moment';
+import 'moment/min/locales';
 
 @Component({
   selector: 'app-nameday',
@@ -11,11 +13,23 @@ export class NamedayComponent implements OnInit {
 
   namesdayData: NameDayData;
   now: number;
-
+  weekDay: String;
+  month: String;
+  date: String;
+  time: String;
+  seconds: String;
+  timePeriod: String;
   constructor(private namesdayService: NamedayService) {
     setInterval(() => {
       this.now = Date.now();
+      this.weekDay = moment().locale('sk').format('dddd, ');
+      this.month = moment().locale('sk').format('MMM ');
+      this.date = moment().locale('sk').format('D, YYYY');
+      this.time = moment().locale('sk').format('h:mm');
+      this.seconds = moment().locale('sk').format('ss');
+      this.timePeriod = moment().locale('sk').format('a');
     }, 1);
+
 
     setInterval(() => this.ngOnInit(), 300000);
   }
